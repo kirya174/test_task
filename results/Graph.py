0abbe4fd534db048d90e829266c91cc8a45fdb13
Kirill <kirya174@gmail.com>
2023-04-01 13:55:20 +0300

    dfs function and tests for dfs finished, gitignore updated, pytest.ini added

--- results/Graph.py
+++ results/Graph.py
@@ -1,61 +1,21 @@
 # Реализовать функцию my_code, которая, используя информацию о смежности узлов графа,
 # распечатывает достижимые вершины, начиная с переданной
 
-def my_code(graph: dict, node: int, visited_nodes=None):
-    print(node)
-    if visited_nodes is None:
-        visited_nodes = [node]
-    for elem in graph[node]:
-        if elem not in visited_nodes:
-            visited_nodes.append(elem)
-            if elem in graph.keys():
-                my_code(graph, elem, visited_nodes)
-            else:
-                print(elem)
-
-
-def DFS(graph: dict,
+def dfs(graph: dict,
         start: int,
         ) -> None:
-    visited_nodes = list()
-    _stack = list()
-    _stack.append(start)
-    while _stack:
-        v = _stack.pop(0)
+
+    visited_nodes = set()
+    stack = [start]
+
+    while stack:
+        v = stack.pop()
 
         if v in visited_nodes:
             continue
-        visited_nodes.append(v)
+        visited_nodes.add(v)
         print(v)
 
-        if v in graph.keys():
-            for node in graph[v]:
-                _stack.append(node)
-
-#
-data = {
-    1: [2, 3],
-    2: [4]
-}
-# my_code(data, 1)
-DFS(data, 1)
-'''
-1
-2
-4
-3
-'''
-
-data = {
-    1: [2, 3],
-    2: [3, 4],
-    4: [1]
-}
-DFS(data, 1)
-
-'''
-1
-2
-3
-4
-'''
+        if v in graph:
+            for node in reversed(graph[v]):
+                stack.append(node)
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,27 +1,66 @@
 import pytest
-from Graph import dfs
+from results.Graph import dfs
 
-def test_simple_graph_dfs(capfd):
-    data = {
-        1: [2, 3],
-        2: [4]
-    }
-    expected = "1\n2\n4\n3\n"
 
-    dfs(data, 1)
-    std_out = capfd.readouterr()
+@pytest.mark.parametrize("data, start, expected",
+                         [
+                             ({
+                                  1: [2, 3],
+                                  2: [4]
+                              },
+                              1,
+                              "1\n2\n4\n3\n"),
 
-    assert std_out.out == expected
+                             ({
+                                  1: [2, 3],
+                                  2: [3, 4],
+                                  4: [1]
+                              },
+                              1,
+                              "1\n2\n3\n4\n"),
 
-def test_recursive_graph_dfs(capfd):
-    data = {
-        1: [2, 3],
-        2: [3, 4],
-        4: [1]
-    }
-    expected = "1\n2\n3\n4\n"
+                             ({
+                                  1: []
+                              },
+                              1,
+                              "1\n"),
 
-    dfs(data, 1)
+                             ({
+                                  1: [2],
+                                  2: [3],
+                                  3: [1]
+                              },
+                              1,
+                              "1\n2\n3\n"),
+
+                             ({
+                                  1: [2],
+                                  2: [1],
+                                  3: [],
+                                  4: []
+                              },
+                              1,
+                              "1\n2\n"),
+                             ({
+                                  1: [2],
+                                  2: [1],
+                                  3: [],
+                                  4: []
+                              },
+                              4,
+                              "4\n"),
+                             ({
+                                  1: [2, 3],
+                                  2: [4],
+                                  3: [4, 5],
+                                  4: [1],
+                                  5: []
+                              },
+                              1,
+                              "1\n2\n4\n3\n5\n"),
+                         ])
+def test_simple_graph_dfs(data, start, expected, capfd):
+    dfs(data, start)
     std_out = capfd.readouterr()
 
     assert std_out.out == expected
