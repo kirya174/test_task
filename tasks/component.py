1a434126c13e9544fda39ab6ec998367f472f13b
Kirill <kirya174@gmail.com>
2023-04-06 16:00:01 +0300

    comments added

--- tasks/component.py
+++ tasks/component.py
@@ -35,6 +35,11 @@ class Component:
                 "Algorithm": algorithms}
 
     def parse_specification(self, object_class) -> set:
+        """
+        returns list of potential paths for provided class according to algorithms specifications
+        :param object_class: object class
+        :return: list of unique potential paths
+        """
         def get_paths(graph: dict, start, path=None) -> list[list]:
             # full path to current node
             path = [start] if path is None else path + [start]
@@ -53,29 +58,41 @@ class Component:
 
         potential_paths = set()
         for algorithm in self.algorithm_list:
+            # getting paths for each algorithm
             paths = get_paths(algorithm.SPECIFICATION, object_class)
+            # converting them to string representation
             str_paths = ['/' + '/'.join([elem.__name__ for elem in path]) for path in paths]
             for path in str_paths:
                 potential_paths.add(path)
         return potential_paths
 
-    def parse_algorithm(self, source_object, algorithm):
+    def parse_algorithm(self, source_object, algorithm) -> dict[dict[list]]:
+        """
+        returns list of actual paths for provided class according to algorithm's implementation
+        :param source_object: object class
+        :param algorithm: algorithm to check path
+        :return: dictionary with occurring paths for this algorithm
+        """
         results = []
         queue = [[source_object()]]
         while queue:
             results.extend(queue)
             new_queue = []
             for item in queue:
+                # Creating list of paths occurring for algorithm
                 new_items = ([*item, new_item] for new_item in algorithm(item[-1]))
                 new_queue.extend(new_items)
             queue = new_queue
 
         result_dict = {}
+        # converting list of paths to dictionary
         for elem in [tuple([self.get_class_name(elem) for elem in result]) for result in results]:
+            # excluding path consisting of 1 element
             if len(elem) > 1:
                 key = '/' + '/'.join(elem[:-1])
                 value = f'{key}/{elem[-1]}'
                 if value not in result_dict.get(key, []):
+                    # add key to dict if it doesn't exist, otherwise append value to dict[key]
                     result_dict.setdefault(key, []).append(value)
 
         return result_dict
